7b534ee636ca336a4e4404a6847d13c69db71707
AnnaLurka <annaklurka@gmail.com>
2020-03-03 08:50:26 +0100

    getAllUsers() method added + minor changes

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,19 +1,27 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.User;
 import com.kodilla.ecommercee.domain.UserDto;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping("v1/user")
+@RequestMapping("/users")
 public class UserController {
 
     private List<UserDto> users = new ArrayList<>();
 
-    @RequestMapping(method = RequestMethod.POST, value = "addUser")
+    @RequestMapping(method = RequestMethod.GET, value = "getAllUsers")
+    public List<UserDto> getAllUsers() {
+        return users;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "addUser", consumes = APPLICATION_JSON_VALUE)
     public UserDto addUser(@RequestBody UserDto userToAdd) {
         users.add(userToAdd);
         return userToAdd;
