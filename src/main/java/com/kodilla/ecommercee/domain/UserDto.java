f59d0aca222d82b38fbe8e232dcf00401c2b8689
AnnaLurka <annaklurka@gmail.com>
2020-03-04 13:11:53 +0100

    JDP200203-5

--- src/main/java/com/kodilla/ecommercee/domain/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -1,20 +1,20 @@
 package com.kodilla.ecommercee.domain;
 
 public final class UserDto {
-    private final Long userId;
+    private final Long id;
     private final String userName;
     private final String lastname;
     private final boolean blocked;
 
-    public UserDto(final Long userId, final String userName, final String lastname, final boolean blocked) {
-        this.userId = userId;
+    public UserDto(final Long id, final String userName, final String lastname, final boolean blocked) {
+        this.id = id;
         this.userName = userName;
         this.lastname = lastname;
         this.blocked = blocked;
     }
 
-    public Long getUserId() {
-        return userId;
+    public Long getId() {
+        return id;
     }
 
     public String getUserName() {
