ccf98a3abc1133e738d7c6a64c262f45347085f3
mackowit <mackowit@o2.pl>
2020-03-06 08:40:45 +0100

    JDP200203-13 Encja Group

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,8 +1,6 @@
 package com.kodilla.ecommercee.domain;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity(name = "groups")
 public class Group {
@@ -10,15 +8,12 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column
     private String name;
-
-    @OneToMany(cascade = CascadeType.ALL,
-                fetch = FetchType.LAZY,
-                mappedBy = "group")
-    private List<Product> groupProductList = new ArrayList<>();
 
     public Group() {
     }
+
     public Group(String name) {
         this.name = name;
     }
@@ -31,19 +26,11 @@ public class Group {
         return name;
     }
 
-    public List<Product> getGroupProductList() {
-        return groupProductList;
-    }
-
-    public void setId(Long id) {
+    private void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
-    }
-
-    public void setGroupProductList(List<Product> groupProductList) {
-        this.groupProductList = groupProductList;
     }
 }
