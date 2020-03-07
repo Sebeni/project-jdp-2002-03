8c12b45563dda62c04789bafa5bac222f53d2214
graczek <raczek.grz@gmail.com>
2020-03-07 19:14:00 +0100

    2nd attempt to fix requested changes at JDP200203-12

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -16,14 +16,6 @@ public class Product {
     @Column
     private String description;
 
-    //@ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
-    //private List<Order> orderList = new ArrayList<>();
-
-    //@ManyToOne
-    //private Group group;
-
-    //private List<Cart> cartList = new ArrayList<>();
-
     public Product() {
     }
 
@@ -34,8 +26,6 @@ public class Product {
     public void setId(Long id) {
         this.id = id;
     }
-
-    //add all args constructor when Group, Order, Cart ready + getters & setters for all
 
     public String getDescription() {
         return description;
