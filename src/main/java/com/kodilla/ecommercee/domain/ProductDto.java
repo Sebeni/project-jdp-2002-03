9222bf5d65e649e68e1791d6a50214cfda7f5c5c
graczek <raczek.grz@gmail.com>
2020-03-10 21:56:05 +0100

    JDP2002-03-12 fixes to ProductDto & ProductRepository

--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -1,13 +1,8 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
 public class ProductDto {
 
     private Long id;
@@ -16,4 +11,34 @@ public class ProductDto {
     private List<Order> orderList = new ArrayList<>();
     private List<Cart> cartList = new ArrayList<>();
 
+    public ProductDto() {
+    }
+
+    public ProductDto(Long id, String description, Group group, List<Order> orderList, List<Cart> cartList) {
+        this.id = id;
+        this.description = description;
+        this.group = group;
+        this.orderList = orderList;
+        this.cartList = cartList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public List<Cart> getCartList() {
+        return cartList;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -3,19 +3,21 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
+@Transactional(readOnly = true)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Override
     List<Product> findAll();
+
     @Override
     Product save(Product product);
+
     @Override
     Optional<Product> findById(Long id);
 
