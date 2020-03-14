6446694eef35496dd96076d6d629012106d38521
graczek <raczek.grz@gmail.com>
2020-03-14 20:46:29 +0100

    JDP2002-03-12 fixes removing lombok from gradle + entity naming

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-@Entity
+@Entity(name = "groups")
 public class Group {
 
     @Id
