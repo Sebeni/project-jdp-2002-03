03c91587269b638a4e31bc376b0339b7b19f71b7
Mateusz Lach <matis_l@tlen.pl>
2020-03-16 16:25:21 +0100

    missing files added

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.domain;
+
+
+import java.util.List;
+
+public class OrderDto {
+    private Long id;
+    private List<String> orderedProductList;
+
+    public OrderDto(Long id, List<String> orderedProductList) {
+        this.id = id;
+        this.orderedProductList = orderedProductList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<String> getOrderedProductList() {
+        return orderedProductList;
+    }
+
+}
