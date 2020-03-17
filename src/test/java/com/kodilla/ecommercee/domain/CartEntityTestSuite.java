2c226e1cc02b9333f714439fe631d718a1d4aeee
Torayasu <torayasuamari@gmail.com>
2020-03-17 22:36:25 +0100

    Synched after Product and Cart Entities merged to master

---
+++ src/test/java/com/kodilla/ecommercee/domain/CartEntityTestSuite.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CartEntityTestSuite {
+
+
+    @Before
+    public void init() {
+
+    }
+
+    @After
+    public void cleanUp() {
+
+    }
+
+    @Test
+    public void shouldAddCarts() {
+        assertTrue(true);
+    }
+
+
+}
