36be752ed25ff0117a097de0771637fadb21ffc4
mackowit <mackowit@o2.pl>
2020-03-20 10:00:50 +0100

    Group Tests changes in init method and delete test

--- src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupTestSuite.java
@@ -24,11 +24,17 @@ public class GroupTestSuite {
     ProductRepository productRepository;
     private Group group1 = new Group("grupa1");
     private Group group2 = new Group("grupa2");
+    private Product product1 = new Product();
+    private Product product2 = new Product();
 
     @Before
     public void init() {
         groupRepository.save(group1);
         groupRepository.save(group2);
+        product1.setGroup(group1);
+        product2.setGroup(group2);
+        productRepository.save(product1);
+        productRepository.save(product2);
     }
 
     @After
@@ -40,21 +46,10 @@ public class GroupTestSuite {
     @Test
     public void testGroupEntitySaveWithProducts() {
         //Given
-        Product product1 = new Product();
-        Product product2 = new Product();
-
-        product1.setGroup(group1);
-        product2.setGroup(group2);
-
         //When
-        productRepository.save(product1);
-        productRepository.save(product2);
-
         //Then
         Assert.assertEquals(2, groupRepository.count());
         Assert.assertEquals(2, productRepository.count());
-        Assert.assertEquals("grupa1", product1.getGroup().getName());
-        Assert.assertEquals("grupa2", product2.getGroup().getName());
     }
 
     @Test
@@ -82,29 +77,35 @@ public class GroupTestSuite {
     @Test
     public void testUpdateGroupName() {
         //Given
-        Product product1 = new Product();
-        product1.setGroup(group1);
-
         //When
-        productRepository.save(product1);
-
         group1.setName("grupa2");
         groupRepository.save(group1);
 
         //Then
-        Assert.assertEquals("grupa2", group1.getName());
-        Assert.assertEquals("grupa2", product1.getGroup().getName());
+        Assert.assertEquals("grupa2", groupRepository.findById(group1.getId()).get().getName());
+        Assert.assertEquals("grupa2", productRepository.findById(product1.getId()).get().getGroup().getName());
     }
 
     @Test
     public void testDeleteGroup() {
         //Given
+        Group group3 = new Group();
+        groupRepository.save(group3);
+
         //When
         Long group1Id = group1.getId();
+        Long group3Id = group3.getId();
 
-        groupRepository.deleteById(group1Id);
+        try {
+            groupRepository.deleteById(group1Id);
+        }
+        catch (Exception e){
+            //do nothing
+        }
+        groupRepository.deleteById(group3Id);
 
         //Then
-        Assert.assertEquals(Optional.empty(), groupRepository.findById(group1Id));
+        Assert.assertTrue(groupRepository.findById(group1Id).isPresent());
+        Assert.assertFalse(groupRepository.findById(group3Id).isPresent());
     }
 }
